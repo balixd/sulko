8e051e3981af71ab329804fcfd3d091c6c14a0d7
Your Name <you@example.com>
2022-11-03 10:37:10 +0100

    filmes

---
+++ NOV/filmek.js
@@ -0,0 +1,6 @@
+const filmek=[
+{"id":"1","műfaj":"Horror","cím":"Spongyabob","évszám":"2015"},
+{"id":"2","műfaj":"vígjáték","cím":"jégvarázs","évszám":"2019"},
+{"id":"3","műfaj":"Romantikus","cím":"MInionok","évszám":"2022"},
+{"id":"4","műfaj":"Akció","cím":"Star wars","évszám":"2005"}
+]
